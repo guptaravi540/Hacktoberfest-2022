98ca12e1d6a5c61ffc2a0dd4a511f7ee855fb6a4
MahnoorShoukat <107980805+MahnoorShoukat@users.noreply.github.com>
2022-10-19 06:39:41 -0700

    Sum of two numbers

---
+++ Sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, add;
+    cout<<"Enter two integers= ";
+    cin>>num1>>num2;
+    add = num1+num2;
+    cout<<"Sum of "<<num1<<" and "<<num2<<"="<<add;
+    cout<<endl;
+    return 0;
+}
